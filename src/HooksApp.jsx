a93207c7566b92f25c129929505cbba2e06db872
joacovieytes <joaquinvieytes92@gmail.com>
2025-07-03 18:02:45 -0300

    Cambio los nombres de los componentes

--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -1,13 +1,13 @@
-import { FetchApp } from "./components/FetchApp"
-import { CounterApp } from "./components/CounterApp"
-import { FormsApp } from "./components/FormsApp"
+import { FetchComponent } from "./components/FetchComponent"
+import { CounterComponent } from "./components/CounterComponent"
+import { FormsComponent } from "./components/FormsComponent"
 
 export const HooksApp = () => {
     return (
         <>
             <h1>Aplicacion de Hooks</h1>
             <hr />
-            <FormsApp/>
+            <FormsComponent/>
         </>
     )
 }
